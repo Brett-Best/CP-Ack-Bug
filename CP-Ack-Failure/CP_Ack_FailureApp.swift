312a318b4d7169b7b805c6eb154dc164c93dbf68
Brett Best <brettbest@live.com.au>
2020-10-04 11:54:38 +1100

    Initial Commit

---
+++ CP-Ack-Failure/CP_Ack_FailureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CP_Ack_FailureApp.swift
+//  CP-Ack-Failure
+//
+//  Created by Brett Best on 4/10/20.
+//
+
+import SwiftUI
+
+@main
+struct CP_Ack_FailureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CP-Ack-FailureTests/CP_Ack_FailureTests.swift
@@ -0,0 +1,33 @@
+//
+//  CP_Ack_FailureTests.swift
+//  CP-Ack-FailureTests
+//
+//  Created by Brett Best on 4/10/20.
+//
+
+import XCTest
+@testable import CP_Ack_Failure
+
+class CP_Ack_FailureTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
